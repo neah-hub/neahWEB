7d6f42a48e5e44ce54b5344982654df278d1a087
Neha Vinjamuri <b24494@students.iitmandi.ac.in>
2026-07-02 20:51:57 +0000

    connecting it to backend code

--- supabase.js
+++ supabase.js
@@ -1,1 +1,13 @@
+import { createClient }
+from "https://esm.sh/@supabase/supabase-js";
 
+const SUPABASE_URL = "https://ralijdslwhmwynvfdcnx.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhbGlqZHNsd2htd3ludmZkY254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwMTE4NTMsImV4cCI6MjA5ODU4Nzg1M30.N98twiwdUU4Yc_oadZQRyZUz-h6OJMxtT6f9EQZct3g";
+
+const supabase = createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+window.supabase = supabase;
